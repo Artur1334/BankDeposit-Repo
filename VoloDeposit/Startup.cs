38c7c1a6c3f7ce6ff3c0b07637e4c29dc6b5b8e5
Artur1334 <artur.h13345@gmail.com>
2017-06-30 14:09:12 +0400

    Add project files.

---
+++ VoloDeposit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VoloDeposit.Startup))]
+namespace VoloDeposit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
