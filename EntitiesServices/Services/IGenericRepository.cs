eb760528c181fcbc7200feaa707bcff28a05ffc9
Artur1334 <artur.h13345@gmail.com>
2017-07-01 21:17:11 +0400

    Entitin,Generic Repositorian interface ,Generic Repository class@ sarqaca arden

---
+++ EntitiesServices/Services/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesServices.Services
+{
+    public interface IGenericRepository<TModel>:IDisposable where TModel : class
+    {
+        IEnumerable<TModel> SelectAll();
+        TModel Select(object id);
+        void Create(TModel item);
+        void Update(TModel item);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ InfrastructureData/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using EntitiesServices.Entities;
+using EntitiesServices.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureData
+{
+    public class GenericRepository<TModel> : IGenericRepository<TModel>  where TModel:class
+    {
+        private ArmDepositEntities DbEntities;
+        private DbSet<TModel> _dbset;
+        public GenericRepository()
+        {
+            this.DbEntities = new ArmDepositEntities();
+            this._dbset = DbEntities.Set<TModel>();
+        }
+
+        public TModel Select(object id)
+        {
+            return _dbset.Find(id);
+        }
+
+        void IGenericRepository<TModel>.Create(TModel entity)
+        {
+            _dbset.Add(entity);
+            DbEntities.SaveChanges();
+        }
+
+        void IGenericRepository<TModel>.Delete(int id)
+        {
+            TModel entityToDelete = _dbset.Find(id);
+            _dbset.Remove(entityToDelete);
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IGenericRepository<TModel>.Save()
+        {
+            try
+            {
+                DbEntities.SaveChanges();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+
+        }
+
+        IEnumerable<TModel> IGenericRepository<TModel>.SelectAll()
+        {
+            return _dbset.ToList();
+        }
+
+        void IGenericRepository<TModel>.Update(TModel entity)
+        {
+            _dbset.Attach(entity);
+
+            DbEntities.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
