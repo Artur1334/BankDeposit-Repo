31cfd60bf703d946db32cef7519fa190d2ce469e
Artur1334 <artur.h13345@gmail.com>
2017-07-15 01:51:04 +0400

    Modal @ Avelacrac

--- VoloDeposit/Controllers/HomeController.cs
+++ VoloDeposit/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using VoloDeposit.ViewModel.Home;
 
 namespace VoloDeposit.Controllers
 {
@@ -47,6 +47,26 @@ namespace VoloDeposit.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        // GET
+        public ActionResult Calculator(string type)
+        {
+            //var calulator = CalculatorResolver.Get(type);
+            //return PartialView(calulator);
+            //CalculatorViewModels cc = new CalculatorViewModels();
+            return View();
+
+        }
+
+        [HttpPost]
+       
+        public ActionResult Calculator([Bind(Include = "Year,Amount")]CalculatorViewModels CalculatotVM)
+        {
+            //var calulator = CalculatorResolver.Get(type);
+            //return PartialView(calulator);
+            //CalculatorViewModels cc = new CalculatorViewModels();
+            return View();
+
         }
     }
 }
---
+++ VoloDeposit/ViewModel/Home/CalculatorViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VoloDeposit.ViewModel.Home
+{
+    public class CalculatorViewModels
+    {
+        public int Year { get; set; }
+        public double Amount { get; set; }
+        public double FinalAmount { get; set; }
+    }
+}
