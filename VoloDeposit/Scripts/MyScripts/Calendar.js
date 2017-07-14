c73b1b23857494dfa1da9c2309f22378e5ec80a8
Artur1334 <artur.h13345@gmail.com>
2017-07-14 21:46:56 +0400

    DatePicker  @ avelacrac

---
+++ VoloDeposit/Scripts/MyScripts/Calendar.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    var min = $('.startdatetimepicker').attr("min");
+    $('.startFromdatetimepicker').datetimepicker({
+        format: 'DD/MM/YYYY'
+    });
+    $('.startTodatetimepicker').datetimepicker({
+        format: 'DD/MM/YYYY'
+    });
+    $('.startdatetimepicker').datetimepicker({
+        format: 'DD/MM/YYYY',
+        minDate: moment()
+    });
+    $('.dateTimepicker').datetimepicker({
+        format: 'DD/MM/YYYY'
+    });
+
+});
